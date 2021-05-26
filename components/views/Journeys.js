6f4aee92529101f81e67980b679ab6aa08795b12
Sharon Kennedy <sharon@nomadiclabs.ca>
2021-05-26 11:21:03 -0400

    journeys page style updates

--- components/views/Journeys.js
+++ components/views/Journeys.js
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react'
 
 const Journey = ({ description, media, prompt, status, image, credit, story_url }) => {
   return (
-    <div className="flex flex-col items-center col-span-3 sm:col-span-1">
+    <div className="flex flex-col items-start justify-start col-span-3 sm:col-span-1">
       <figure>
-        <img src={image[0].url} alt={description} className="w-full h-auto object-cover border border-black" />
-        <figcaption className="mt-1">{`Credit: ${credit}`}</figcaption>
+        <img src={image[0].url} alt={description} className="w-full h-auto object-cover" />
+        <figcaption className="mt-1 px-5 sm:px-0">{credit}</figcaption>
       </figure>
       {story_url &&
-        <a href={story_url} target="_blank" rel="noopener noreferrer">Read the full story</a>
+        <a href={story_url} target="_blank" rel="noopener noreferrer" className="px-5 sm:px-0">Read the full story</a>
       }
     </div>
   )
@@ -42,12 +42,17 @@ const Journeys = () => {
 
   return (
     <div className="flex-grow overflow-auto">
-      <div className="container mx-auto px-5 py-8">
-        <h2 className="hidden sm:block text-3xl mb-5">Journeys</h2>
-        <p className="mb-2">Share your creative journeys with #DirectionsToNowhere on your favorite (social) media channels.</p>
-        <p className="mb-5">Want to be featured on here? <Link href="/submit"><a>Send us your creation.</a></Link></p>
+      <div className="container mx-auto py-8">
+        <div className="px-5">
+          <h2 className="hidden sm:block text-3xl mb-5">Journeys</h2>
+          <p className="mb-2">Share your creative journeys with <span className="text-green font-bold">#DirectionsToNowhere</span> on your favorite (social) media channels.</p>
+          <p className="mb-2">Want to be featured on here?</p>
+          <Link href="/submit">
+            <a className="text-center text-green border border-green px-2 py-1 btn inline-flex items-center mb-5">Send us your creation</a>
+          </Link>
+        </div>
         { journeys &&
-          <div className="grid gap-4 grid-cols-3 my-4">
+          <div className="grid gap-4 grid-cols-3 my-4 sm:px-5">
             {
               journeys.map(({id, fields}) => <Journey key={id} id={id} {...fields} />)
             }
