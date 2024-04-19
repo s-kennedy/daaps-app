2a03051c70cf27a5cf3474102adee4bffc40383c
Sharon <sharon.peishan.kennedy@gmail.com>
2024-04-19 17:25:02 -0400

    specify node version

--- utils/airtableOps.js
+++ utils/airtableOps.js
@@ -1,6 +1,6 @@
 import Airtable from 'airtable'
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+const base = new Airtable({ apiKey: process.env.AIRTABLE_TOKEN }).base(
     process.env.AIRTABLE_BASE_ID
 );
 
